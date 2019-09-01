d8a0966b673fbb73cbb196647912ac9f68df6f55
闪电金刚之葫芦娃 <1402141265@qq.com>
2019-09-01 21:17:06 +0800

    添加头部和脚步组件

---
+++ components/footer.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="footer">
+    <el-row type="flex" class="foot" justify="space-between">
+      <el-col :span="6">
+        <dl>
+          <dt>闲云旅游旅游网</dt>
+          <dd>上亿旅行者共同打造的"旅行神器"</dd>
+          <dd>
+            <span>60,000</span> 多个全球旅游目的地
+          </dd>
+          <dd>
+            <span>600,000</span> 个细分目的地新玩法
+          </dd>
+          <dd>
+            <span>760,000,000</span> 次攻略下载
+          </dd>
+          <dd>
+            <span>38,000</span> 家旅游产品供应商
+          </dd>
+        </dl>
+      </el-col>
+      <el-col :span="5">
+        <dl>
+          <dt>关于我们</dt>
+          <dd>隐私政策 商标声明</dd>
+          <dd>服务协议 游记协议</dd>
+          <dd>商城平台服务协议</dd>
+          <dd>网络信息侵权通知指引</dd>
+          <dd>闲云旅游旅游网服务监督员</dd>
+          <dd>网站地图加入闲云旅游</dd>
+        </dl>
+      </el-col>
+      <el-col :span="5">
+        <dl>
+          <dt>旅行服务</dt>
+          <dd>旅游攻略 酒店预订</dd>
+          <dd>旅游特价 国际租车</dd>
+          <dd>旅游问答 旅游保险</dd>
+          <dd>旅游指南 订火车票</dd>
+          <dd>旅游资讯 APP下载</dd>
+        </dl>
+      </el-col>
+      <el-col :span="8">
+        <div class="erweima">
+          <img src="http://157.122.54.189:9093/images/1556522965.png" alt />
+          <span>关注我们</span>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row type="flex" class="copy" justify="center">
+        <span>京ICP备08001421号 京公网安备110108007702 Copyright © 2016-2019 博学谷 All Rights Reserved</span>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+.footer {
+  background-color: rgb(51, 51, 51);
+  color: #ccc;
+  .foot {
+    width: 1000px;
+    margin: 0 auto;
+    padding: 30px 0 30px 50px;
+    border-bottom: 1px solid #ccc;
+    dd {
+      font-size: 12px;
+      margin-top: 10px;
+      span {
+        color: rgb(242, 154, 33);
+      }
+    }
+    .erweima {
+      width: 140px;
+      text-align: center;
+      padding-left: 50px;
+      img{
+          width: 100%;
+      }
+      span{
+          font-size: 12px;
+          
+      }
+    }
+  }
+  .copy{
+      padding: 50px 0;
+      span{
+          font-size: 12px;
+      }
+  }
+}
+</style>
---
+++ components/header.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="header">
+    <el-row type="flex" class="main" justify="space-between">
+      <div class="logo">
+        <nuxt-link to="/">
+          <img src="http://157.122.54.189:9093/images/logo.jpg" alt />
+        </nuxt-link>
+      </div>
+      <el-row type="flex" class="navs">
+        <nuxt-link to="/">首页</nuxt-link>
+        <nuxt-link to="/post">旅游攻略</nuxt-link>
+        <nuxt-link to="/hotel">酒店</nuxt-link>
+        <nuxt-link to="/air">国内机票</nuxt-link>
+      </el-row>
+      <div class="login">
+          <nuxt-link to="/user/login">登录 / 注册</nuxt-link>
+      </div>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+    .header{
+        height: 60px;
+        border-bottom: 3px solid #eee;
+        box-shadow: 0 2px 2px #eee;
+        .main{
+            width: 1000px;
+            margin:0 auto;
+            .logo{
+                width: 156px;
+                img{
+                    width: 100%;
+                    margin-top: 9px;
+                }
+            }
+            .navs{
+                flex: 1;
+                line-height: 60px;
+                margin: 0 20px;
+                a{
+                    padding: 0 20px;
+                    box-sizing: border-box;
+                    display: block;
+                    height:60px;
+                    &:hover,&:active{
+                        color:#409eff;
+                        border-bottom:5px solid #409eff;
+                    }
+                    &:focus{
+                        background-color: #409eff;
+                        color:#fff;
+                    }
+                }
+            }
+            .login{
+                line-height: 60px;
+            }
+        }
+    }
+</style>
